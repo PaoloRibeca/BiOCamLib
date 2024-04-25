7ed9fbb73e1e7ea031305c449893a13573103c5c
Paolo Ribeca <paolo.ribeca@gmail.com>
2024-04-25 09:04:28 +0100

    Finished implementation of Consensus.of_alignment

--- lib/Consensus.ml
+++ lib/Consensus.ml
@@ -88,80 +88,147 @@ include (
         res;
       Buffer.contents buf
     (* Processes a string.t array *)
-    exception IncompatibleLengthOnLine of int
-    let of_alignment ?(tip_gap_multiplier = 2.5) ?(max_tip_threshold = 30) ?(min_branch_threshold = 40) al =
+    let of_alignment
+        ?(tip_gap_multiplier = 2.5) ?(max_tip_threshold = 30) ?(min_branch_threshold = 40)
+        ?(consensus_window = 7) ?(min_coverage = 5) al =
       if tip_gap_multiplier < 0. then
         Printf.sprintf "(%s): Negative value %g to argument 'tip_gap_multiplier'" __FUNCTION__ tip_gap_multiplier
           |> failwith;
       check_percentage "max_tip_threshold" max_tip_threshold;
       check_percentage "min_branch_threshold" min_branch_threshold;
-      let len =
-        if Array.length al > 0 then
-          String.length al.(0)
-        else
-          0 in
-      let al =
-        Array.mapi
-          (fun i seq ->
-            if String.length seq <> len then
-              IncompatibleLengthOnLine i |> raise;
-            Sequences.Lint.dnaize ~keep_lowercase:false ~keep_dashes:true seq
-              |> remove_tips ~tip_gap_multiplier ~max_tip_threshold |> Bytes.of_string)
-          al in
-      (* We replace stretches of dashes on the sides with spaces and compute coverage *)
-      let cov = Array.make len 0 in
-      Array.iter
-        (fun seq ->
-          let first_non_dash_idx = ref 0 in
-          while !first_non_dash_idx < len && seq.Bytes.@(!first_non_dash_idx) = '-' do
-            seq.Bytes.@(!first_non_dash_idx) <- ' ';
-            incr first_non_dash_idx
-          done;
-          let last_non_dash_idx = len - 1 |> ref in
-          while !last_non_dash_idx >= 0 && seq.Bytes.@(!last_non_dash_idx) = '-' do
-            seq.Bytes.@(!last_non_dash_idx) <- ' ';
-            decr last_non_dash_idx
-          done;
-          for i = !first_non_dash_idx to !last_non_dash_idx do
-            cov.(i) <- cov.(i) + 1            
-          done)
-        al;
-      let max_res = Array.make len 0 and res = Bytes.make len 'n' in
-      (* We instate gaps *)
-      for i = 0 to len - 1 do
-        (* We compute the most frequent character in the column *)
-        let stats = ref CharMap.empty in
+      if consensus_window < 1 then
+        Printf.sprintf "(%s): Argument 'consensus_window' must be positive (found %d)" __FUNCTION__ consensus_window
+          |> failwith;
+      if min_coverage < 0 then
+        Printf.sprintf "(%s): Negative value %d to argument 'min_coverage'" __FUNCTION__ min_coverage |> failwith;
+      let n_seqs = Array.length al in
+      if n_seqs = 0 then
+        ""
+      else begin
+        let seq_len = String.length al.(0) in
+        if consensus_window < 0 || consensus_window > seq_len then
+          Printf.sprintf "(%s): Argument 'consensus_window' must be non-negative and no greater than alignment length"
+            __FUNCTION__ |> failwith;
+        let al =
+          Array.mapi
+            (fun i seq ->
+              if String.length seq <> seq_len then
+                Printf.sprintf "(%s): Incompatible sequence length on line %d (expected %d, found %d)"
+                  __FUNCTION__ i seq_len (String.length seq)
+                |> failwith;
+              Sequences.Lint.dnaize ~keep_lowercase:false ~keep_dashes:true seq
+                |> remove_tips ~tip_gap_multiplier ~max_tip_threshold |> Bytes.of_string)
+            al in
+        (* We replace stretches of dashes on the sides with spaces and compute coverage *)
+        let cov = Array.make seq_len 0 in
         Array.iter
           (fun seq ->
-            let c = seq.Bytes.@(i) in
-            match CharMap.find_opt c !stats with
-            | None ->
-              stats := CharMap.add c (ref 1) !stats
-            | Some n ->
-            incr n)
+            let first_non_dash_idx = ref 0 in
+            while !first_non_dash_idx < seq_len && seq.Bytes.@(!first_non_dash_idx) = '-' do
+              seq.Bytes.@(!first_non_dash_idx) <- ' ';
+              incr first_non_dash_idx
+            done;
+            let last_non_dash_idx = seq_len - 1 |> ref in
+            while !last_non_dash_idx >= 0 && seq.Bytes.@(!last_non_dash_idx) = '-' do
+              seq.Bytes.@(!last_non_dash_idx) <- ' ';
+              decr last_non_dash_idx
+            done;
+            for i = !first_non_dash_idx to !last_non_dash_idx do
+              cov.(i) <- cov.(i) + 1            
+            done)
           al;
-        let max_n = ref 0 and max_c = ref 'n' in
-        CharMap.iter
-          (fun c n ->
-            if !n > !max_n then begin
-              max_n := !n;
-              max_c := c
-            end)
-          !stats;
-        let max_n = !max_n and max_c = !max_c in
-        if max_c = '-' && 100 * max_n >= min_branch_threshold * cov.(i) then begin
-          max_res.(i) <- max_n;
-          res.Bytes.@(i) <- max_c
-        end
-      done;
-      (* We compute the consensus by sliding k-mer window *)
-
-
-
-
-
-
-      Array.map Bytes.to_string al
+        let max_res = Array.make seq_len 0 and res = Bytes.make seq_len 'n' in
+        (* We instate gaps *)
+        for i = 0 to seq_len - 1 do
+          (* We compute the most frequent character in the column *)
+          let stats = ref CharMap.empty in
+          Array.iter
+            (fun seq ->
+              let c = seq.Bytes.@(i) in
+              match CharMap.find_opt c !stats with
+              | None ->
+                stats := CharMap.add c (ref 1) !stats
+              | Some n ->
+                incr n)
+            al;
+          let max_n = ref 0 and max_c = ref 'n' in
+          CharMap.iter
+            (fun c n ->
+              if !n > !max_n then begin
+                max_n := !n;
+                max_c := c
+              end)
+            !stats;
+          let max_n = !max_n in
+          if !max_c = '-' && 100 * max_n >= min_branch_threshold * cov.(i) then begin
+            max_res.(i) <- max_n;
+            res.Bytes.@(i) <- '-'
+          end
+        done;
+        (* We compute the consensus by sliding k-mer window *)
+        let kmers =
+          Array.init n_seqs
+            (fun i ->
+              Bytes.sub_string al.(i) 0 consensus_window |> KMers.SlidingWindow.make) in
+        let n_spaces =
+          Array.init n_seqs
+            (fun i ->
+              let kmer = KMers.SlidingWindow.contents kmers.(i) and n = ref 0 in
+              String.iter
+                (function
+                  | ' ' -> incr n
+                  | _ -> ())
+                kmer;
+              !n) in
+        for i_col = 0 to seq_len - consensus_window do
+          (* We determine the most frequent k-mer at this position *)
+          let stats = ref StringMap.empty in
+          Array.iteri
+            (fun i_seq kmer ->
+              (* We update k-mer and number of spaces *)
+              if i_col > 0 then begin
+                let c = al.(i_seq).Bytes.@(consensus_window + i_col - 1) in
+                let old_c = KMers.SlidingWindow.add_char kmer c in
+                if old_c = ' ' then
+                  n_spaces.(i_seq) <- n_spaces.(i_seq) - 1;
+                if c = ' ' then
+                  n_spaces.(i_seq) <- n_spaces.(i_seq) + 1
+              end;
+              let k = KMers.SlidingWindow.contents kmer in
+              (* Side k-mers containing spaces are not taken into account *)
+              if n_spaces.(i_seq) <> 0 then begin
+                match StringMap.find_opt k !stats with
+                | None ->
+                  stats := StringMap.add k (ref 1) !stats
+                | Some n ->
+                  incr n
+              end)
+            kmers;
+          let max_n = ref 0 and max_k = ref "" in
+          StringMap.iter
+            (fun k n ->
+              if !n > !max_n then begin
+                max_n := !n;
+                max_k := k
+              end)
+            !stats;
+          let max_n = !max_n in
+          String.iteri
+            (fun i c ->
+              let i_c = i_col + i in
+              if max_n > max_res.(i_c) && 100 * max_n >= min_branch_threshold * cov.(i) then begin
+                max_res.(i) <- max_n;
+                res.Bytes.@(i) <-
+                  (* The case conveys information about coverage *)
+                  if max_n >= min_coverage then
+                    Char.uppercase_ascii c
+                  else
+                    Char.lowercase_ascii c
+              end)
+            !max_k
+        done;
+        Bytes.to_string res
+      end
   
     (* PUBLIC *)
     module MpileupLine:
@@ -185,8 +252,8 @@ include (
           eliminate terminal alignment segment if its number of non-gaps is no more than
           max_tip_threshold / 100 of the total number of non-gaps in the line *)
     val remove_tips: ?tip_gap_multiplier:float -> ?max_tip_threshold:int -> string -> string
-    exception IncompatibleLengthOnLine of int
-    val of_alignment: ?tip_gap_multiplier:float -> ?max_tip_threshold:int -> ?min_branch_threshold:int -> string array -> string array
+    val of_alignment: ?tip_gap_multiplier:float -> ?max_tip_threshold:int -> ?min_branch_threshold:int ->
+                      ?consensus_window:int -> ?min_coverage:int -> string array -> string
   
 
 
